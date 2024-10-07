03f00901f329543e1530fa0840fc4fdec8199d64
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-10-08 00:56:22 +0300

    feat: ValidateLoginInput middleware

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,9 +3,10 @@ import { register, login } from "../controllers/authController.js";
 const router = Router();
 
 import { validateRegisterInput } from "../middleware/validationMiddleware.js";
+import { validateLoginInput } from "../middleware/validationMiddleware.js";
 
 
 router.post("/register", validateRegisterInput, register);
-router.post("/login", login);
+router.post("/login", validateLoginInput, login);
 
 export default router;
