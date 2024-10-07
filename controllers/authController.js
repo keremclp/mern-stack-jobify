94193df7a9180def09cf5c5978a7cbe925885de1
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-10-08 00:52:37 +0300

    feat: hash password logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,17 @@
 import { StatusCodes } from "http-status-codes";
+import { hashPassword } from "../utils/passwordUtils.js";
+
 import User from "../models/UserModel.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 export const login = async (req, res) => {
   res.send("login");
