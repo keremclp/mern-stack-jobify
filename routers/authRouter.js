e0ab67b34cf57abfbfe2be504b5656ac856dd9de
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-10-08 16:12:56 +0300

    feat: logout user

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 const router = Router();
 
 import { validateRegisterInput } from "../middleware/validationMiddleware.js";
@@ -8,5 +8,6 @@ import { validateLoginInput } from "../middleware/validationMiddleware.js";
 
 router.post("/register", validateRegisterInput, register);
 router.post("/login", validateLoginInput, login);
+router.get("/logout", logout);
 
 export default router;
