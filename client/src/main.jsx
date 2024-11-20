d1116ed2b10b9f08da25adbd13c9c382b5c3bf08
keremclp <kerem00962@gmail.com>
2024-11-20 19:04:31 +0300

    feat: customFetch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-fetch('/api/v1/test')
-  .then((res) => res.json())
-  .then((data) => console.log(data))
 
 
 createRoot(document.getElementById('root')).render(
