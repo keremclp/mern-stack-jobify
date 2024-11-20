257fe0696b6f0358e1a6f5fb360a31c490821d6c
keremclp <kerem00962@gmail.com>
2024-11-20 18:26:37 +0300

    feat: change the port prpoperties

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5100/api',
+        target: 'http://localhost:5400/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
